97a2ea0609e9c0bbd08c77269f365225beb9369e
pceuropa <info@pceuropa.net>
2017-01-13 22:53:24 +0000

    datetime migration command execution

--- migrations/menu_create.sql
+++ migrations/menu_create.sql
@@ -1,8 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.6.4deb1+deb.cihar.com~xenial.1
--- https://www.phpmyadmin.net/
--- Generation Time: Oct 01, 2016 at 06:31 PM
--- Server version: 10.1.17-MariaDB-1~xenial
+-- #Copyright (c) 2017 Rafal Marguzewicz pceuropa.net
 -- PHP Version: 7.0.8-0ubuntu0.16.04.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -10,8 +6,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `menu` (
 `menu_id` int(11) NOT NULL,
-  `menu` text NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `menu` text NOT NULL,
+  `menu_name` char(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 ALTER TABLE `menu` ADD PRIMARY KEY (`menu_id`);
 ALTER TABLE `menu` MODIFY `menu_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+ALTER TABLE `menu` ADD PRIMARY KEY (`menu_id`), ADD KEY `unique-index-menu-menu_name` (`menu_name`);
