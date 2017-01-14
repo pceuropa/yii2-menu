9f8c914ced476c284495cbb2e5790f2be98b36fa
pceuropa <info@pceuropa.net>
2017-01-14 12:51:23 +0000

    hide icons change hierarchy inputs in _form

--- views/creator/_form.php
+++ views/creator/_form.php
@@ -11,7 +11,6 @@ $this->registerJs(
     window.glyphicons = {$glyphicons};
     ",
     \yii\web\View::POS_HEAD);
-
 ?>
 
 
@@ -30,33 +29,6 @@ $this->registerJs(
 		</div>
 	</div>
 
-    <div class="form-group" id="icon-box">
-        <label for="icon" class="col-sm-3 control-label">Icon</label>
-        <div class="col-sm-7">
-            <?= \kartik\select2\Select2::widget([
-                'name' => 'icon',
-                'data' => \pceuropa\menu\icons\Glyphicons::forge()->getDropDownOptions(),
-                'options' => ['placeholder' => 'Select an icon ...', 'id' => 'icon'],
-                'pluginOptions' => [
-                    'escapeMarkup' => new \yii\web\JsExpression("function(m) { return m; }"),
-                    'allowClear' =>  true
-                ],
-            ]); ?>
-        </div>
-    </div>
-
-	<div id="location-box" class="form-group">
-	<label class="col-sm-3 control-label">Location</label>
-	<div class="col-sm-8">
-		<?= BaseHtml::dropDownList(
-			'',
-			'left',
-			['left' => 'Navbar Left', 'right' => 'Navbar Right'],
-			['id' => 'location', 'class' => 'form-control input-sm']
-		); ?>
-	</div>
-	</div>
-
 	<div id="anchor-box" class="form-group">
     <label class="col-sm-3 control-label">Label</label>
     <div class="col-sm-8">
@@ -71,6 +43,35 @@ $this->registerJs(
 	</div>
 	</div>
 	
+	
+	<div id="location-box" class="form-group">
+	<label class="col-sm-3 control-label">Location</label>
+	<div class="col-sm-8">
+		<?= BaseHtml::dropDownList(
+			'',
+			'left',
+			['left' => 'Navbar Left', 'right' => 'Navbar Right'],
+			['id' => 'location', 'class' => 'form-control input-sm']
+		); ?>
+	</div>
+	</div>
+<!--	
+	<div class="form-group" id="icon-box">
+        <label for="icon" class="col-sm-3 control-label">Icon</label>
+        <div class="col-sm-7">
+            <?= \kartik\select2\Select2::widget([
+                'name' => 'icon',
+                'data' => \pceuropa\menu\icons\Glyphicons::forge()->getDropDownOptions(),
+                'options' => ['placeholder' => 'Select an icon ...', 'id' => 'icon'],
+                'pluginOptions' => [
+                    'escapeMarkup' => new \yii\web\JsExpression("function(m) { return m; }"),
+                    'allowClear' =>  true
+                ],
+            ]); ?>
+        </div>
+    </div>
+ -->   
+    
 	<div class="form-group">
     <label class="col-sm-3 control-label"></label>
     <div class="col-sm-8">
@@ -80,6 +81,8 @@ $this->registerJs(
 		); ?>
 	</div>
 	</div>
+	
+	
 </div>
 	<ul id="edit" class="col-md-2 well">Drop here to edit</ul>
 	<ul id="trash" class="col-md-2 well">Drop here to trash</ul>
